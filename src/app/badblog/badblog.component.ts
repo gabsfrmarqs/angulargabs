d72b649523602745b4b858f5da1d252a2e8983f3
Gabriel Marques <gabrielfrmarques@outlook.com>
2025-03-20 00:36:09 -0300

    I failed changing the favicon. Also stupid idea for a blog

---
+++ src/app/badblog/badblog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-badblog',
+  standalone: true,
+  imports: [],
+  templateUrl: './badblog.component.html',
+  styleUrl: './badblog.component.scss'
+})
+export class BadblogComponent {
+
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { LeekspinComponent } from '../leekspin/leekspin.component';
 import { RouterModule } from '@angular/router';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -11,4 +12,18 @@ import { RouterModule } from '@angular/router';
 })
 
 export class HomeComponent {
+  constructor(private meta: Meta){
+    this.setMetaTags();
+  }
+
+  setMetaTags(){
+      this.meta.updateTag({ name: 'description', content: 'idk which field is this one!!' });
+      this.meta.updateTag({ property: 'og:title', content: 'wow! embed! cool!' });
+      this.meta.updateTag({ property: 'og:site_name', content: 'multimeter fanclub' });
+      this.meta.updateTag({ property: 'og:description', content: 'Descrição legal!!' });
+      this.meta.updateTag({ property: 'og:type', content: 'website' });
+      this.meta.updateTag({ property: 'og:image', content: 'assets/images/icon.png' });
+      this.meta.updateTag({ property: 'og:image:width', content: '256' });
+      this.meta.updateTag({ property: 'og:image:height', content: '256' });
+  }
 }
